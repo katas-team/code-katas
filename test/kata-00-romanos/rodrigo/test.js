0c28e8120750ef09bad11d7748beaf71f04a550f
jrvldam <jrvldam@gmail.com>
2017-03-01 20:00:42 +0100

    Tests recursivos

---
+++ test/kata-00-romanos/rodrigo/test.js
@@ -0,0 +1,10 @@
+const assert = require('assert')
+const index = require('../../../katas/kata-01-romanos/rodrigo/index')
+
+describe('Test demo', function () {
+
+  it('Canary test',  function ()  {
+    assert.equal(index, 'hello');
+  });
+
+});
