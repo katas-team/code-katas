ebb234899efd9fd486769f0432c027e77745f6b0
jrvldam <jrvldam@gmail.com>
2017-03-09 00:41:29 +0100

    rodrigo kata romanos pasa los tests

--- katas/kata-01-romanos/rodrigo/index.js
+++ katas/kata-01-romanos/rodrigo/index.js
@@ -1,1 +1,63 @@
-module.exports = 'hello'
+module.exports = function (number) {
+  if (typeof number === 'number') {
+    const SIMPLE_ROMANS = {
+      1: 'I',
+      5: 'V',
+      10: 'X',
+      50: 'L',
+      100: 'C',
+      500: 'D',
+      1000: 'M'
+    };
+
+    if (SIMPLE_ROMANS[number]) return SIMPLE_ROMANS[number];
+
+    var numbers = factorize({number: number, factors: [1000, 100, 10, 1]});
+
+    numbers = numbers.map(function (number) {
+      return SIMPLE_ROMANS[number];
+    });
+
+    return numbers.join('');
+  }
+};
+
+function factorize({number, factors}) {
+  const EXCEPT_ROMANS = {
+    5: 'V',
+    50: 'L',
+    500: 'D'
+  };
+  const EXCEPT_FACTORS = [500, 50 , 5 , 0];
+  var _number = number;
+  var numbers = [];
+
+  factors.forEach(function (factor, i, arr) {
+    var loop = Math.floor(_number / factor);
+
+    if (loop < 4 || factor === 1000) {
+      for (var j = 0; j < loop; ++j) {
+        numbers.push(factor);
+        _number -= factor;
+      }
+    }
+
+    var mod = Math.floor(_number % EXCEPT_FACTORS[i]);
+
+    if (_number === mod && _number >= (EXCEPT_FACTORS[i] - (factor / 10))) {
+      numbers.push((factor / 10), EXCEPT_FACTORS[i]);
+      _number -= EXCEPT_FACTORS[i] - (factor / 10);
+    }
+    if (mod < _number && mod >= (EXCEPT_FACTORS[i] - (factor / 10)) && mod >= (EXCEPT_FACTORS[i] - 2 * (factor / 10))) {
+      numbers.push((factor / 10), factor);
+      _number -= factor - (factor / 10);
+      mod = _number;
+    }
+    if (mod < _number) {
+      numbers.push(EXCEPT_FACTORS[i]);
+      _number -= EXCEPT_FACTORS[i];
+    }
+  });
+
+  return numbers;
+}
--- test/kata-00-romanos/rodrigo/test.js
+++ test/kata-00-romanos/rodrigo/test.js
@@ -1,10 +1,19 @@
 const assert = require('assert')
-const index = require('../../../katas/kata-01-romanos/rodrigo/index')
+const toRomans = require('../../../katas/kata-01-romanos/rodrigo/index')
 
-describe('Test demo', function () {
+describe('arabic to romans', function () {
 
-  it('Canary test',  function ()  {
-    assert.equal(index, 'hello');
-  });
-
-});
+  it('1 returns I', () => assert.equal(toRomans(1), 'I'))
+  it('50 returns L', () => assert.equal(toRomans(50), 'L'))
+  it('20 returns XX', () => assert.equal(toRomans(20), 'XX'))
+  it('300 returns CCC', () => assert.equal(toRomans(300), 'CCC'))
+  it('222 returns CCXXII', () => assert.equal(toRomans(222), 'CCXXII'))
+  it('11 returns XI', () => assert.equal(toRomans(11), 'XI'))
+  it('70 returns LXX', () => assert.equal(toRomans(70), 'LXX'))
+  it('40 returns XL', () => assert.equal(toRomans(40), 'XL'))
+  it('44 returns XLIV', ()=> assert.equal(toRomans(44), 'XLIV'))
+  it('394 returns CCCXCIV', () => assert.equal(toRomans(394), 'CCCXCIV'))
+  it('39 returns XXXIX', () => assert.equal(toRomans(39), 'XXXIX'))
+  it('47 returns XLVII', () => assert.equal(toRomans(47), 'XLVII'))
+  it('4457 returns MMMMCDLVII', () => assert.equal(toRomans(4457), 'MMMMCDLVII'))  
+})
