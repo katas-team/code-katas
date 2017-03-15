2071f9070c4eba89eb9622a938424d7aff67f63c
jheredia <jheredia@80soluciones.com>
2017-03-15 10:12:15 +0100

    firts commit about test

--- katas/kata-01-romanos/jose/index.js
+++ katas/kata-01-romanos/jose/index.js
@@ -1,7 +1,7 @@
 var n = ['I', 'IV', 'V', 'IX', 'X', 'XL', 'L', 'XC', 'C', 'CD', 'D', 'CM', 'M'];
 var v = [1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000];
 
-function conv(x) {
+module.exports = function (x) {
 	if( x<1 || x>3999 ) {	// Un simple control de error
 		return (x + ' no valido');
 	}
