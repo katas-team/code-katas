b7048ae30b7556395fd9f5fda5d9e3231135cdc1
jheredia <jheredia@80soluciones.com>
2017-03-15 10:04:48 +0100

    clean nombres

--- test/kata-01-romanos/jose/jose_test_01.js
+++ test/kata-01-romanos/jose/jose_test_01.js
@@ -1,7 +1,7 @@
 const assert = require('assert')
 const toRomans = require('../../../katas/kata-01-romanos/jose/index')
 
-describe('arabic to romans', function () {
+describe('Prueba numeros romanos', function () {
 
   it('1 returns I', () => assert.equal(toRomans(1), 'I'))
 })
